0015bb5eb1a0b6441a88301e5e530de3c7368f62
Sagar Morvadiya <morvadiyasagar99@gmail.com>
2020-07-24 17:42:42 +0530

    "changes of db and bar add"

--- routes/vechical.js
+++ routes/vechical.js
@@ -43,59 +43,68 @@ router.post('/create', function(req, res, next) {
     //array[0].substring(0,3);
     first = "MH02CL";
     second = "0555";
+    var bar = new Promise((resolve, reject) => {
 
-    for (var i = 0; i < array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
 
-        var vechicalno = array[i].substring(1, array[i].length - 1);
-
-
-        var sql1 = 'SELECT * FROM ?? WHERE vechical_no = ?';
-        var tbl = 'vechicalinfo';
-
-        var query = connection.query(sql1, [tbl, vechicalno], (err, results, fileds) => {
-            console.log(results);
-            if (err) {
-                console.log(err);
-            } else {
+            var vechicalno = array[i].substring(1, array[i].length - 1);
 
 
-                cnt = results.length;
+            var sql1 = 'SELECT * FROM ?? WHERE vechical_no = ?';
+            var tbl = 'vechicalinfo';
+
+            var query = connection.query(sql1, [tbl, vechicalno], (err, results, fileds) => {
+                console.log(results);
+                if (err) {
+                    console.log(err);
+                } else {
 
 
-                if (cnt > 0) {
-                    if (results[0].company == "Tata") {
-                        var rsult = {};
-                        rsult['vechical_no'] = results[0].vechical_no;
-                        rsult['data'] = 'Not Allowed';
+                    cnt = results.length;
 
-                        list.push(rsult);
+
+                    if (cnt > 0) {
+                        if (results[0].company == "Tata") {
+                            var rsult = {};
+                            rsult['vechical_no'] = results[0].vechical_no;
+                            rsult['data'] = 'Not Allowed';
+
+                            list.push(rsult);
+
+                        } else {
+                            var rsult = {};
+                            rsult['vechical_no'] = results[0].vechical_no;
+                            rsult['chase_no'] = results[0].chase_no;
+                            rsult['company'] = results[0].company;
+                            rsult['model'] = results[0].model;
+                            list.push(rsult);
+
+                        }
+                        console.log("list", list);
+
+
 
                     } else {
                         var rsult = {};
-                        rsult['vechical_no'] = results[0].vechical_no;
-                        rsult['chase_no'] = results[0].chase_no;
-                        rsult['company'] = results[0].company;
-                        rsult['model'] = results[0].model;
+                        rsult['vechical_no'] = vechicalno;
+                        rsult['data'] = 'Data Not Found';
+
                         list.push(rsult);
-
                     }
-
-
-
-                } else {
-                    var rsult = {};
-                    rsult['vechical_no'] = vechicalno;
-                    rsult['data'] = 'Data Not Found';
-
-                    list.push(rsult);
                 }
-            }
+            });
+            if (i == array.length - 1)
+                resolve();
+        }
+        console.log('for list', list);
+    });
+
+    bar.then(() => {
+        console.log("bar then", list);
+        res.json({
+            msg: list
         });
-    }
-
-    // res.json({
-
-    // });
+    });
 
     return res.send(list);
 
@@ -175,81 +184,86 @@ router.post('/subscribe', function(req, res, next) {
         }
 
     })
-    var i = 0;
+    var bar = new Promise((resolve, reject) => {
+        var i = 0;
 
-    for (i = 0; i < array.length; i++) {
+        for (i = 0; i < array.length; i++) {
 
-        for (var j = 0; j < opration.length; j++) {
-            var cnt = 0;
-            var urlinfo = '';
-            if (i == 0) {
-                var ayy = array[i].substring(0, array[i].length - 1);
-            } else {
-                var ayy = array[i].substring(1, array[i].length - 1);
-            }
-            if (j == 0) {
-                var opp = opration[j].substring(0, opration[j].length - 1)
-
-            } else {
-                var opp = opration[j].substring(1, opration[j].length - 1)
-
-            }
-
-            var sql1 = 'SELECT * FROM ?? WHERE vechical_no = ? AND opration = ?';
-            var tbl = 'subscribe_detalis';
-            var query = connection.query(sql1, [tbl, ayy, opp], (err, results, fileds) => {
-                console.log(results);
-                if (err) {
-                    console.log(err);
+            for (var j = 0; j < opration.length; j++) {
+                var cnt = 0;
+                var urlinfo = '';
+                if (i == 0) {
+                    var ayy = array[i].substring(0, array[i].length - 1);
                 } else {
+                    var ayy = array[i].substring(1, array[i].length - 1);
+                }
+                if (j == 0) {
+                    var opp = opration[j].substring(0, opration[j].length - 1)
 
+                } else {
+                    var opp = opration[j].substring(1, opration[j].length - 1)
 
-                    cnt = results.length;
+                }
 
-                    if (cnt > 0) {
-                        console.log('from here' + results[0].url);
-                        var rsult = {};
-                        rsult['url'] = results[0].url;
-                        rsult['opration'] = opp;
-                        rsult['vechicalno'] = ayy;
-                        list.push(rsult);
-
-                        var rsult = {};
-                        rsult['url'] = url;
-                        rsult['opration'] = opp;
-                        rsult['vechicalno'] = ayy;
-                        list.push(rsult);
-                        console.log("sdfd", list.length, list);
+                var sql1 = 'SELECT * FROM ?? WHERE vechical_no = ? AND opration = ?';
+                var tbl = 'subscribe_detalis';
+                var query = connection.query(sql1, [tbl, ayy, opp], (err, results, fileds) => {
+                    console.log(results);
+                    if (err) {
+                        console.log(err);
                     } else {
 
-                        var sql = `INSERT INTO subscribe_detalis (vechical_no, opration, url) VALUES ("${ayy}", "${opp}", "${url}")`;
-                        connection.query(sql, function(err, result) {
-                            if (err) {
-                                res.status(500).send({ error: 'Something failed!' })
-                            }
 
-                        })
+                        cnt = results.length;
+
+                        if (cnt > 0) {
+                            console.log('from here' + results[0].url);
+                            var rsult = {};
+                            rsult['url'] = results[0].url;
+                            rsult['opration'] = opp;
+                            rsult['vechicalno'] = ayy;
+                            list.push(rsult);
+
+                            var rsult = {};
+                            rsult['url'] = url;
+                            rsult['opration'] = opp;
+                            rsult['vechicalno'] = ayy;
+                            list.push(rsult);
+                            console.log("sdfd", list.length, list);
+                        } else {
+
+                            var sql = `INSERT INTO subscribe_detalis (vechical_no, opration, url) VALUES ("${ayy}", "${opp}", "${url}")`;
+                            connection.query(sql, function(err, result) {
+                                if (err) {
+                                    res.status(500).send({ error: 'Something failed!' })
+                                }
+
+                            })
+                        }
                     }
-                }
-            })
+                })
+            }
+
         }
+    });
 
-    }
-
-    console.log("form listinfo");
-    console.log("finallist" + list);
-    if (i == array.length) {
+    bar.then(() => {
+        console.log("form listinfo");
+        console.log("finallist" + list);
+        // if (i == array.length) {
         console.log('list' + i)
         res.json({
             msg: "Successfully Subscribe",
             data: list
         });
-    } else {
-        res.json({
-            msg: "Successfully Subscribed",
-            data: list
-        });
-    }
+        // } else {
+        //     res.json({
+        //         msg: "Successfully Subscribed",
+        //         data: list
+        //     });
+        // }
+    });
+
 
 
 
@@ -284,6 +298,7 @@ router.get('/getcall', function(req, res, next) {
                     res.on('data', (d) => {
 
                         process.stdout.write(d);
+
 
 
                     });
